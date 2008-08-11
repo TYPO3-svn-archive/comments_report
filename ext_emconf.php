de6facf802a47a07506b4b60c4882e3e149a1d5f
dmitry <dmitry@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-08-11 09:23:35 +0000

    1.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/comments_report/trunk@11066 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "comments_report"
 #
-# Auto generated 24-06-2008 18:43
+# Auto generated 11-08-2008 12:23
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.1.0',
+	'version' => '1.0.0',
 	'constraints' => array(
 		'depends' => array(
 			'comments' => '',
@@ -39,7 +39,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:16:{s:9:"ChangeLog";s:4:"2e80";s:32:"class.tx_commentsreport_hook.php";s:4:"6ed4";s:12:"ext_icon.gif";s:4:"7c67";s:17:"ext_localconf.php";s:4:"7109";s:14:"ext_tables.php";s:4:"f860";s:13:"locallang.xml";s:4:"fe25";s:16:"locallang_db.xml";s:4:"33f8";s:19:"doc/wizard_form.dat";s:4:"bbd4";s:20:"doc/wizard_form.html";s:4:"b780";s:35:"pi1/class.tx_commentsreport_pi1.php";s:4:"3187";s:17:"pi1/locallang.xml";s:4:"7eff";s:14:"res/styles.css";s:4:"6a92";s:17:"res/template.html";s:4:"f7ad";s:22:"res/template_email.txt";s:4:"7a01";s:36:"static/comments_report/constants.txt";s:4:"d26c";s:32:"static/comments_report/setup.txt";s:4:"2b42";}',
+	'_md5_values_when_last_written' => 'a:15:{s:9:"ChangeLog";s:4:"5cef";s:32:"class.tx_commentsreport_hook.php";s:4:"6ed4";s:12:"ext_icon.gif";s:4:"7c67";s:17:"ext_localconf.php";s:4:"7109";s:14:"ext_tables.php";s:4:"f860";s:13:"locallang.xml";s:4:"fe25";s:16:"locallang_db.xml";s:4:"33f8";s:14:"doc/manual.sxw";s:4:"90f7";s:35:"pi1/class.tx_commentsreport_pi1.php";s:4:"d835";s:17:"pi1/locallang.xml";s:4:"7eff";s:14:"res/styles.css";s:4:"6a92";s:17:"res/template.html";s:4:"f7ad";s:22:"res/template_email.txt";s:4:"cade";s:36:"static/comments_report/constants.txt";s:4:"d26c";s:32:"static/comments_report/setup.txt";s:4:"2b42";}',
 	'suggests' => array(
 	),
 );
